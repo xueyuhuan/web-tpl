671a18d1c7a3590aba97c36d18456efa455f23a7
xueyuhuan <501946152@qq.com>
2019-07-19 09:40:10 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  lintOnSave: false
+    publicPath:process.env.NODE_ENV === 'production' ? '/' :'/web-tpl/',
+    lintOnSave: false,
+    chainWebpack:config => {
+        config.plugins.delete('prefetch')
+    },
+    css:{
+        loaderOptions:{
+            sass:{
+                data:`@import "@/assets/scss/_mixin.scss";`
+            }
+        }
+    },
+    devServer:{
+        port:719,
+    }
 }
